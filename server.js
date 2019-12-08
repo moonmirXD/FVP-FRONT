047ef377513cc6188e2df594fafdc85b0511d95d
Ryan <arquezaryan@yahoo.com>
2019-12-08 11:42:01 +0800

    debugging

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/fvp-front"));
+app.use(express.static(__dirname + "/dist/FVP-Front"));
 
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname + "/dist/fvp-front/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/FVP-Front/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
